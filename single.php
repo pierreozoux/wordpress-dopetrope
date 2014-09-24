4412deaeb5763f8fada2fbaaf5cf01dc9e6c6325
pierreozoux <pierre.ozoux@seedrs.com>
2014-09-24 13:49:57 +0100

    Adds dynamic sidebar for single post

--- single.php
+++ single.php
@@ -10,29 +10,12 @@
     
       <!-- Sidebar -->
         <section class="box">
-          <a href="#" class="image featured"><img src="<?php bloginfo('template_directory'); ?>/images/pic09.jpg" alt="" /></a>
-          <header>
-            <h3>Sed etiam lorem nulla</h3>
-          </header>
-          <p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
-          <footer>
-            <a href="#" class="button alt">Magna sed taciti</a>
-          </footer>
-        </section>
-        <section class="box">
-          <header>
-            <h3>Feugiat consequat</h3>
-          </header>
-          <p>Veroeros sed amet blandit consequat veroeros lorem blandit adipiscing et feugiat sed lorem consequat feugiat lorem dolore.</p>
-          <ul class="divided">
-            <li><a href="#">Sed et blandit consequat sed</a></li>
-            <li><a href="#">Hendrerit tortor vitae sapien dolore</a></li>
-            <li><a href="#">Sapien id suscipit magna sed felis</a></li>
-            <li><a href="#">Aptent taciti sociosqu ad litora</a></li>
-          </ul>
-          <footer>
-            <a href="#" class="button alt">Ipsum consequat</a>
-          </footer>
+          <!– WP LOOP STARTS/ENDS –>
+          <?php $my_query = new WP_Query('category_name=flyer&showposts=6'); ?>
+          <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
+            <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(array(367, 255), array( 'class' => 'sidebar' )); ?></a>
+          <?php endwhile; ?>
+          <!– WP LOOP STARTS/ENDS –>
         </section>
 
     </div>
